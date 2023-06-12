027caa731b0fc7201958fac0d30437ed66acc597
Pedro Assunção <73478225+PedroHenAssuncao@users.noreply.github.com>
2023-06-12 20:12:54 -0300

    Correções acesso a dados, agora utilizam await

--- src/js/acessoDados/notificacoes.js
+++ src/js/acessoDados/notificacoes.js
@@ -2,85 +2,85 @@ const URL = 'http://localhost:3000/notificacoes'
 const URLusuarios = 'http://localhost:3000/usuarios'
 
 // Metodo que retorna todos os notificacoes do banco
-function getAllNotificacoes() {
-    fetch(URL)
-        .then(response => response.json())
-        .then(data => {
-            // Aqui estão os dados do arquivo JSON
-            console.log(data);
-            return data;
-        })
-        .catch(error => {
-            console.error('Erro ao acessar banco:', error);
-        });
+async function getAllNotificacoes() {
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Erro ao acessar banco:', error);
+    }
 }
 
 // Metodo que retorna o notificacao que possui o Id especificado
-function getNotificacaoById(id) {
-    fetch(URL + '/' + id)
-        .then(response => response.json())
-        .then(data => {
-            // Aqui estão os dados do arquivo JSON
-            console.log(data);
-            return data;
-        })
-        .catch(error => {
-            console.error('Erro ao acessar banco:', error);
-        });
+async function getNotificacaoById(id) {
+    try {
+        const response = await fetch(URL + '/' + id);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Erro ao acessar banco:', error);
+    }
 }
 
 // Metodo que deleta o notificacao indicado pelo id
-function deleteNotificacao(id){
-    fetch(URL + '/' + id, {
-        method: 'DELETE',
-    })
-    .then(response => response.json())
-    .then(() => location.reload())
-    .catch(error => {
+async function deleteNotificacao(id) {
+    try {
+        const response = await fetch(URL + '/' + id, {
+            method: 'DELETE',
+        });
+        // const data = await response.json();
+        // return data;
+        location.reload()
+    } catch (error) {
         console.error('Erro ao acessar banco:', error);
-    });
+    }
 }
 
 // Metodo que adiciona o notificacao ao banco
-function createNotificacao(usuario){
-    fetch(URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: usuario
-    })
-    .then(response => response.json())
-    .then(() => location.reload())
-    .catch(error => {
+async function createNotificacao(notificacao) {
+    try {
+        const response = await fetch(URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: notificacao
+        });
+        // const data = await response.json();
+        // return data;
+        location.reload()
+    } catch (error) {
         console.error('Erro ao acessar banco:', error);
-    });
+    }
 }
 
 // Metodo que atualiza um notificacao já existente
-function updateNotificacao(usuario){
-    fetch(URL, {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: usuario
-    })
-    .then(response => response.json())
-    .then(() => location.reload())
-    .catch(error => {
+async function updateNotificacao(notificacao) {
+    try {
+        const response = await fetch(URL, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: notificacao
+        });
+        // const data = await response.json();
+        // return data;
+        location.reload()
+    } catch (error) {
         console.error('Erro ao acessar banco:', error);
-    });
+    }
 }
 
 //Metodo que busca todos os notificacaos que pertecem ao usuário
-function getNotificacaoByUserId(userId){
-    fetch(URL)
-        .then(response => response.json())
-        .then(data => {
-            return data.filter(objeto => objeto.usuarioId === userId);
-        })
-        .catch(error => {
-            console.error('Erro ao acessar banco:', error);
-        });
+async function getNotificacaoByUserId(userId) {
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        const filteredData = data.filter(objeto => objeto.usuarioId === userId);
+        return filteredData;
+    } catch (error) {
+        console.error('Erro ao acessar banco:', error);
+    }
 }
--- src/js/acessoDados/reservas.js
+++ src/js/acessoDados/reservas.js
@@ -2,94 +2,97 @@ const URL = 'http://localhost:3000/reservas'
 const URLusuarios = 'http://localhost:3000/usuarios'
 
 // Metodo que retorna todos os reservas do banco
-function getAllReservas() {
-    fetch(URL)
-        .then(response => response.json())
-        .then(data => {
-            return data;
-        })
-        .catch(error => {
-            console.error('Erro ao acessar banco:', error);
-        });
+async function getAllReservas() {
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Erro ao acessar banco:', error);
+    }
 }
 
 // Metodo que retorna a reserva que possui o Id especificado
-function getReservaById(id) {
-    fetch(URL + '/' + id)
-        .then(response => response.json())
-        .then(data => {
-            return data;
-        })
-        .catch(error => {
-            console.error('Erro ao acessar banco:', error);
-        });
+async function getReservaById(id) {
+    try {
+        const response = await fetch(URL + '/' + id);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Erro ao acessar banco:', error);
+    }
 }
 
 // Metodo que deleta a reserva indicado pelo id
-function deleteReserva(id){
-    fetch(URL + '/' + id, {
-        method: 'DELETE',
-    })
-    .then(response => response.json())
-    .then(() => {
+async function deleteReserva(id) {
+    try {
+        const response = await fetch(URL + '/' + id, {
+            method: 'DELETE',
+        });
+        // const data = await response.json();
+        // return data;
         location.reload()
-    }).catch(error => {
+    } catch (error) {
         console.error('Erro ao acessar banco:', error);
-    });
+    }
 }
 
 // Metodo que adiciona a reserva ao banco
-function createReserva(usuario){
-    fetch(URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: usuario
-    })
-    .then(response => response.json())
-    .then(() => location.reload())
-    .catch(error => {
+async function createReserva(usuario) {
+    try {
+        const response = await fetch(URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: usuario
+        });
+        // const data = await response.json();
+        // return data;
+        location.reload()
+    } catch (error) {
         console.error('Erro ao acessar banco:', error);
-    });
+    }
 }
 
 // Metodo que atualiza uma reserva já existente
-function updateReserva(usuario){
-    fetch(URL, {
-        method: 'PATCH',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: usuario
-    })
-    .then(response => response.json())
-    .then(() => location.reload())
-    .catch(error => {
+async function updateReserva(usuario) {
+    try {
+        const response = await fetch(URL, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: usuario
+        });
+        // const data = await response.json();
+        // return data;
+        location.reload()
+    } catch (error) {
         console.error('Erro ao acessar banco:', error);
-    });
+    }
 }
 
 //Metodo que busca todos os reservas que pertecem ao usuário
-function getReservasByUserId(userId){
-    fetch(URL)
-        .then(response => response.json())
-        .then(data => {
-            return data.filter(objeto => objeto.usuarioId === userId);
-        })
-        .catch(error => {
-            console.error('Erro ao acessar banco:', error);
-        });
+async function getReservasByUserId(userId) {
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        const filteredData = data.filter(objeto => objeto.usuarioId === userId);
+        return filteredData;
+    } catch (error) {
+        console.error('Erro ao acessar banco:', error);
+    }
 }
 
 //Metodo que busca todos os reservas que foram feitas na loja
-function getReservasByLojaId(lojaId){
-    fetch(URL)
-        .then(response => response.json())
-        .then(data => {
-            return data.filter(objeto => objeto.lojaId === lojaId);
-        })
-        .catch(error => {
-            console.error('Erro ao acessar banco:', error);
-        });
+async function getReservasByLojaId(lojaId) {
+    try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        const filteredData = data.filter(objeto => objeto.lojaId === lojaId);
+        return filteredData;
+    } catch (error) {
+        console.error('Erro ao acessar banco:', error);
+    }
 }
