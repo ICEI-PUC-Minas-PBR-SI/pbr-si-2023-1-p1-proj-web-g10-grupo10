e8aca2607cc706e6cef61ede6a5dfdf8aa530482
arthuragp <aagpinto@sga.pucminas.br>
2023-06-20 19:03:50 -0300

    datatable adicionado à tela de produto

--- src/js/acessoDados/produtos.js
+++ src/js/acessoDados/produtos.js
@@ -92,4 +92,4 @@ async function getProdutoByUserId(userId) {
         console.error('Erro ao acessar banco:', error);
     }
 }
-export { getAllProdutos, getProdutoById, deleteProduto, createProduto, updateProduto, getProdutoByUserId }
+// export { getAllProdutos, getProdutoById, deleteProduto, createProduto, updateProduto, getProdutoByUserId }
