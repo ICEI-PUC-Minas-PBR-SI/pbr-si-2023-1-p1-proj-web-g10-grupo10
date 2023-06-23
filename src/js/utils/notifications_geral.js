f61890a8d5a5a9cad09533bf7c655320be87fc70
isaac_portela <kroosbr278@gmail.com>
2023-06-22 23:44:01 -0300

    feat: add notificacao generica

---
+++ src/js/utils/notifications_geral.js
@@ -0,0 +1,17 @@
+/*
+Os tipos da biblioteca toastr são: success, info, warning e error.
+*/
+
+function exibirNotificacao(titulo, mensagem, tipo) {
+  toastr.options = {
+    closeButton: true,
+    progressBar: true,
+    positionClass: 'toast-top-right',
+    timeOut: 5000
+  };
+  
+  // passando o tipo como objeto
+  toastr[tipo](mensagem, titulo);
+}
+
+export { exibirNotificacao };
